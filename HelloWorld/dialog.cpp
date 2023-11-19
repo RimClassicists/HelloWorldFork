59664ab1a97a860fc07275c511fe7fb5eb6ddf2a
RimClassicists <zero23rim@gmail.com>
2023-11-19 14:19:55 +0800

    first commit

---
+++ HelloWorld/dialog.cpp
@@ -0,0 +1,15 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+
+Dialog::Dialog(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+}
+
+Dialog::~Dialog()   // 析构函数
+{
+    delete ui;
+}
+
